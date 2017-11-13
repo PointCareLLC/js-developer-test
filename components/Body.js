ad749a0a27b49d5c8496572e2aed614176263010
Combiz Saleh <combiz.s@gmail.com>
2017-11-13 11:54:01 -0800

    Added: display list of manifest json;

--- components/Body.js
+++ components/Body.js
@@ -3,6 +3,8 @@ import React from 'react';
 export default class Body extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = { manifest: [], isLoading: true };
     this.fetchManifest = this.fetchManifest.bind(this);
   }
 
@@ -13,10 +15,35 @@ export default class Body extends React.Component {
   async fetchManifest() {
     const res = await fetch('/api/manifest');
     const manifest = await res.json();
-    this.setState({ manifest });
+    this.setState({ manifest, isLoading: false });
+  }
+
+  displayList = (list) => {
+    return <ol className='list'>{
+      list.map(item => {
+        return this.listItem(item)
+      })
+    }</ol>
+  }
+
+  listItem = item => {
+    return (
+      <li key={item.id} className='item'>
+        <div className='make'>{item.make}</div>
+        <div className='model'>{item.model}</div>
+        <div className='color'>{item.color}</div>
+        <div className='enteredAt'>{item.enteredAt}</div>
+      </li>
+    )
+  }
+
+  loading() {
+    return <div>Loading</div>
   }
 
   render() {
-    return <div>hello world!</div>;
+    const { isLoading, manifest } = this.state;
+
+    return isLoading ? this.loading() : this.displayList(manifest);
   }
 }
