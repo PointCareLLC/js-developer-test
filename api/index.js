c67aab26f4b85d45a28099d9d45bc5067d855c09
Combiz Saleh <combiz.s@gmail.com>
2017-11-13 10:34:41 -0800

    Added: route for /api/manifest;

--- api/index.js
+++ api/index.js
@@ -2,5 +2,13 @@
 
 const express = require('express');
 const app = express();
+const manifestRoute = express.Router();
+const manifest = require('./manifest.json')
+
+manifestRoute.get('/', (req, res) => {
+	res.send(manifest)
+})
+
+app.use('/manifest', manifestRoute)
 
 module.exports = app;
